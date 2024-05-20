3feeca69eb9a053b76b869f365ebee4be30461ff
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2024-05-20 11:10:27 +0200

    find distinct sensor counts for both plants regarding inverters and other measuring equipment

---
+++ standard_exercise/6_query.sql
@@ -0,0 +1,11 @@
+SELECT COUNT(DISTINCT source_key) AS distinct_Inverters,
+PLANT_ID
+FROM `sql-sandbox-20240505.SolarPower.Generation_Data`
+GROUP BY PLANT_ID;
+-- 22 per plant
+
+SELECT COUNT(DISTINCT source_key) AS distinct_Inverters,
+PLANT_ID
+FROM `sql-sandbox-20240505.SolarPower.Weather_Sensor_Data`
+GROUP BY PLANT_ID;
+-- 1
