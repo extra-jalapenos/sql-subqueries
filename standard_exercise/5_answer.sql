7bc88a9fe2d23fc65e1a68feaeaf78d3dc59a542
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2024-05-20 11:07:38 +0200

    add explanation to query 4's values

---
+++ standard_exercise/5_answer.sql
@@ -0,0 +1,4 @@
+-- 5. What can you say about the hourly distribution of power generated? Why are there zeros in the resulting table?
+-- solar plants like full solar radiation mostly when they're not hot/ air is not hot yet, which explains why the midday hours aren't the most efficient
+
+-- I also don't have zeroes in my table?
